6214ee0d66e847919060459c0a07880d2ab5dfee
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-04-28 23:11:35 +0500

    added the cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 mongoose.connect('mongodb://localhost:27017/blog', {useNewUrlParser: true})
 
